10d34456989ccd3298925ddceffd02070a4c08c4
Jacob <jacobescarcha@gmail.com>
2021-11-22 23:30:30 -0800

    Second Page is added in this commit, almost included the 3rd page but decided to do it in a branch

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function cleanUpIndex() {
     let elements = document.querySelectorAll('.contact');
     for (let i = 0; i < elements.length; i++) {
         elements[i].remove();
-
     }
 };
-
 
 
 function createSingleIndex() {
@@ -25,7 +23,6 @@ function createSingleIndex() {
 };
 
 
-
 let contactList = [
     {
       name: "Yo quiero taco bell",
@@ -40,7 +37,6 @@ let contactList = [
       email: "whatsup@doc.example.com",
     },
 ];
-
 
 
 function renderIndex(contact) {
@@ -60,5 +56,84 @@ function renderIndex(contact) {
         main[0].appendChild(elementA);
     }
 };
+
+
+
+
+
+function cleanUpView() {
+    let contactinfo = document.querySelectorAll('.contactinfo');
+    contactinfo[0].remove();    
+}
+
+
+function renderView(contact) {
+    let main = document.querySelectorAll('.main')[0];
+    let divInfo = document.createElement('div');
+    let divName = document.createElement('div');
+    let divImg = document.createElement('img');
+    let divEmail = document.createElement('div');
+    let divPhone = document.createElement('div');
+    let divAddress = document.createElement('div');
+    let divButtons = document.createElement('div');
+    let buttonEdit = document.createElement('button')
+    let buttonClose = document.createElement('button')
+
+
+    divImg.src = './img/profile.jpg';
+    divImg.classList.add('profilepic');
+    divImg.alt = 'Profile Picture';
+
+
+    divName.append(contact.name);
+    divName.classList.add('contactname');
+
+
+    divInfo.classList.add('contactinfo');
+
+
+    divName.appendChild(divImg);
+    divInfo.appendChild(divName);
+    main.appendChild(divInfo);
+
+
+    divEmail.append('email: ', contact.email);
+    divEmail.classList.add('contactemail');
+
+
+    divInfo.appendChild(divEmail);
+
+
+    divPhone.append('cell: ', contact.phone);
+    divPhone.classList.add('contactphone');
+
+
+    divInfo.appendChild(divPhone);
+
+    
+    divAddress.append('address: ', contact .address);
+    divAddress.classList.add('contactaddress');
+
+
+    divInfo.appendChild(divAddress);
+
+
+    buttonEdit.classList.add('button');
+    buttonEdit.classList.add('edit');
+    buttonEdit.value = 'Edit';
+    buttonEdit.append('Edit');
+
+    buttonClose.classList.add('button');
+    buttonClose.classList.add('close');
+    buttonClose.value = 'Close';
+    buttonClose.append('Close');
+
+    divButtons.classList.add('buttons');
+    divButtons.appendChild(buttonEdit);
+    divButtons.appendChild(buttonClose);
+
+
+    divInfo.appendChild(divButtons);
+}
 
 
